e71949c4f489bdf9941e69cc96bd57e057703673
gitfname <newamailnam@gmail.com>
2023-08-16 17:02:50 +0430

    start coding

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-slate-900 grid place-items-center">
+      <p className="text-3xl text-white/90 font-medium text-center tracking-wide">Start Coding</p>
+    </div>
+  )
+}
+
+export default App
